e90d98ee7afb58732bc9771032d3797922ecac23
SferrellaA <alexander@sferrella.com>
2024-01-26 16:27:00 -0500

    cleaned up mess from making last commit work

--- test11.py
+++ test11.py
@@ -1,7 +1,6 @@
 from network_manager import NetworkManager
 import uasyncio
 from display import *
-#from time import sleep
 
 display = LED_Strip(50)
 wifi = NetworkManager("Tigers99", "Tigers99")
@@ -16,34 +15,24 @@ async def timer(time, command, *args):
 
 async def part1():
     # Startup
-    #await timer(3, display.rainbow)
+    await timer(3, display.rainbow)
     connect = uasyncio.create_task(wifi.connect())
     
     # The regular loop
     while True:
+        # Display the ssid
         display.flash(0,0,0)
-        global checkPointOne
-        await display.moorse("I")
+        await display.moorse(wifi._ssid)
         
-        if wifi._sta_if.isconnected():
+        # Check connection *after* the ssid is displayed
+        if wifi._sta_if.isconnected(): #success!
             connect.cancel()
             print(f"\nConnected to {wifi._ssid}")
             await timer(3, display.rainbow)
             return
-        else:
+        else: # not connected
             print(f"Could not connect to {wifi._ssid}")
             await timer(3, display.fire)
-        '''        
-        uasyncio.gather(wifi.connect())
-        task = uasyncio.create_task(display.flash_loop())
-        connection = await wifi.connect()
-        task.cancel()
-        
-        # Connection Successfuul
-        if connection:
-            display.flash(0,0,0)
-            return
-        else:
-        '''
+
 uasyncio.run(part1())
 print("here")
