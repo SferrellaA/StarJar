a40914655a39cad05d95630690143518444caef0
SferrellaA <alexander@sferrella.com>
2024-03-02 13:34:56 -0600

    cleaned up what files are kept in the web folder

--- web/server.py
+++ web/server.py
@@ -1,35 +1,24 @@
 from bottle import route, run, static_file
 from os import path
 
-interpreter = open(path.join('brython', 'brython.js')).read()
+interpreter = open('brython.js').read()
 @route('/brython.js')
 def brython():
     return interpreter
 
 template = open('template.html').read()
-def build_template(*args):
-    content = ""
-    for arg in args:
-        content += f'<script type="text/python" src="{arg}"></script>'
-    return template.format(content=content)
-
 @route('/')
 def index():
-    return build_template('/brython/index.py', '/brython/css.py')
-
-@route('/index.html')
-def index_html():
-    return open("index.html").read()
+    return template.format(content=open('index.py').read())
 
 @route('/images/<figure>')
 def images(figure):
     return static_file(figure, root="images")
 
-@route('/brython/<script>')
+@route('/<script>')
 def get_script(script):
-    full_path = path.join("brython", script)
-    if path.exists(full_path):
-        return open(full_path).read()
+    if path.exists(script):
+        return open(script).read()
     return "404"
 
 run(host='localhost', port=8080, debug=True)
